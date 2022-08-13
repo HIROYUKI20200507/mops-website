fc461259f6d1c8c1e5c5ffa9082270c91892f6ec
HIROYUKI20200507 <dokomademo1124@gmail.com>
2022-08-13 17:49:58 +0900

    update: 管理画面の設定調整

--- mops-website/functions.php
+++ mops-website/functions.php
@@ -22,12 +22,45 @@ function codex_custom_init()
     'rewrite' => ['slug' => 'news'],
     'capability_type' => 'post',
     // 'has_archive' => true,
-    'menu_position' => null,
+    'menu_position' => 5,
+    'menu_icon' => 'dashicons-welcome-write-blog',
     'supports' => ['title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments'],
   ];
 
   register_post_type('news', $args);
+
+  $labels = [
+    'name' => '求人情報',
+    'singular_name' => '求人情報',
+    'edit_item' => '求人追加',
+    'parent_item_colon' => '',
+    'menu_name' => '求人情報',
+  ];
+
+  $args = [
+    'labels' => $labels,
+    'public' => true,
+    'publicly_queryable' => true,
+    'show_ui' => true,
+    'show_in_menu' => true,
+    'hierarchical' => true,
+    'query_var' => true,
+    'rewrite' => ['slug' => 'recruit'],
+    'capability_type' => 'post',
+    'menu_position' => 5,
+    'menu_icon' => 'dashicons-groups',
+    'supports' => ['title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments'],
+  ];
+
+  register_post_type('recruit', $args);
 }
 add_action('init', 'codex_custom_init');
 
+/*【管理画面】投稿メニューを非表示 */
+function remove_menus()
+{
+  global $menu;
+  remove_menu_page('edit.php'); // 投稿を非表示
+}
+add_action('admin_menu', 'remove_menus');
 ?>
