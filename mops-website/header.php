2b5acab8df95bd7d8f711203a1c6b6f9656efedd
HIROYUKI20200507 <dokomademo1124@gmail.com>
2022-09-04 11:05:40 +0900

    update: OGP設定

--- mops-website/header.php
+++ mops-website/header.php
@@ -10,4 +10,13 @@
 
     <!-- ファビコン -->
     <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/logo/mops_logo_favicon.png">
+
+    <!-- OGP -->
+    <meta property="og:url" content="ページURL" />
+    <meta property="og:title" content="株式会社MOPS | 公式ホームページ" />
+    <meta property="og:type" content="website" />
+    <meta property="og:description" content="株式会社MOPS | 公式ホームページです。カジママという家事代行サービスを運営しております。" />
+    <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/assets/images/service.png" />
+    <meta property="og:site_name" content="株式会社MOPS" />
+    <meta property="og:local" content="ja_JP" />
 </head>
