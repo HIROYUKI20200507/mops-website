9d948f253d3aeff10c298a9e5548e9def59b7920
HIROYUKI20200507 <dokomademo1124@gmail.com>
2022-09-13 22:50:58 +0900

    add database archives

--- mops-website/functions.php
+++ mops-website/functions.php
@@ -53,14 +53,41 @@ function codex_custom_init()
   ];
 
   register_post_type('recruit', $args);
+
+  $labels = [
+    'name' => 'お知らせ',
+    'singular_name' => 'お知らせ',
+    'edit_item' => 'お知らせ追加',
+    'parent_item_colon' => '',
+    'menu_name' => 'お知らせ',
+  ];
+
+  $args = [
+    'labels' => $labels,
+    'public' => true,
+    'publicly_queryable' => true,
+    'show_ui' => true,
+    'show_in_menu' => true,
+    'hierarchical' => true,
+    'query_var' => true,
+    'rewrite' => ['slug' => 'info'],
+    'capability_type' => 'post',
+    'menu_position' => 5,
+    'menu_icon' => 'dashicons-groups',
+    'supports' => ['title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments'],
+  ];
+
+  register_post_type('info', $args);
 }
 add_action('init', 'codex_custom_init');
 
-/*【管理画面】投稿メニューを非表示 */
-function remove_menus()
-{
-  global $menu;
-  remove_menu_page('edit.php'); // 投稿を非表示
-}
-add_action('admin_menu', 'remove_menus');
+// /*【管理画面】投稿メニューを非表示 */
+// function remove_menus()
+// {
+//   global $menu;
+//   remove_menu_page('edit.php'); // 投稿を非表示
+// }
+// add_action('admin_menu', 'remove_menus');
+//
+
 ?>
